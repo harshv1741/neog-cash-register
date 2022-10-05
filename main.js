92410d61af594622e9a13f68f55315fffba38e68
harshv1741 <ardostree2001@outlook.com>
2022-10-05 10:56:20 +0530

    "After 2.5 weeks its Finally Working!"

---
+++ main.js
@@ -0,0 +1,88 @@
+const billAmt = document.querySelector("#bill-amt");
+const cashReceived = document.querySelector("#user-cash");
+const submit = document.querySelector("#submit");
+const msg = document.querySelector("#output");
+const changeSection = document.querySelectorAll(".changeCount");
+
+const availableChange = [2000, 500, 100];
+
+const showMsg = (text, sound_selector) => {
+  const infoSE = document.querySelector("#infoSE");
+  const failSE = document.querySelector("#failSE");
+  const successSE = document.querySelector("#successSE");
+  const resetSE = document.querySelector("#resetSE");
+
+  switch (sound_selector) {
+    case "info":
+      msg.style.color = "blue";
+      msg.innerText = text;
+      infoSE.play();
+      break;
+
+    case "success":
+      msg.style.color = "green";
+      msg.innerText = text;
+      successSE.play();
+      break;
+
+    case "fail":
+      msg.style.color = "red";
+      msg.innerText = text;
+      failSE.play();
+      break;
+
+    case "reset":
+      msg.innerText = "";
+      billAmt.innerText = "";
+      cashReceived.innerText = "";
+      resetSE.play();
+      break;
+
+    default:
+      console.log("No Message");
+      break;
+  }
+};
+
+submit.addEventListener("click", () => {
+  bill = Number(billAmt.value);
+  cashGiven = Number(cashReceived.value);
+
+  const calculateChange = (changeAmt) => {
+    for (let i = 0; i < availableChange.length; i++) {
+      const changeCount = Math.trunc(changeAmt / availableChange[i]);
+      changeAmt %= availableChange[i];
+      changeSection[i].innerText = changeCount;
+    }
+  };
+
+  if (isNaN(bill) || isNaN(cashGiven)) {
+    showMsg("Please! Only enter number in the box.", "fail");
+  } else {
+    if (bill && cashGiven) {
+      //   console.log("Bill Ammount" + bill);
+      //   console.log("CashGiven" + cashGiven);
+      if (bill > 0) {
+        if (cashGiven >= bill) {
+          const change = cashGiven - bill;
+          calculateChange(change);
+        } else {
+          text = "🍽️ Plates are waiting for you to wash them";
+          showMsg(text, "fail");
+        }
+      } else {
+        text = "Invalid bill ammount";
+        showMsg(text, "fail");
+      }
+    } else {
+      showMsg("Enter both the numbers", "info");
+    }
+  }
+});
+
+reset.addEventListener("click", () => {
+  showMsg("", "reset");
+  for (let i = 0; i < changeSection.length; i++) {
+    changeSection[i].innerText = "0";
+  }
+});
